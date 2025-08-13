6d082967f62412d87f429d2939af45173ec7991d
YASH PAL <2004yashpal@gmail.com>
2025-08-14 00:24:13 +0530

    chat-gpt update2

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'chat_screen.dart';
 import 'theme_provider.dart';
-import 'setting_page.dart'; // Import your settings page
+import 'package:mitra/setting_page.dart'; // Import your settings page
 import 'package:flutter/material.dart';
 
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
